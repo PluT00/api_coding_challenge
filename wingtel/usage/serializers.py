bab21aa28ed69a75a9dec1d880d64cede856a484
PluT00 <valeran2003@mail.ru>
2020-05-19 19:07:06 +0300

    Add Data and Voice usage record serializers

--- wingtel/usage/serializers.py
+++ wingtel/usage/serializers.py
@@ -2,6 +2,20 @@ from rest_framework import serializers
 from wingtel.usage.models import DataUsageRecord, VoiceUsageRecord
 
 
+class DataUsageRecordSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = DataUsageRecord
+        fields = '__all__'
+
+
+class VoiceUsageRecord(serializers.ModelSerializer):
+
+    class Meta:
+        model = VoiceUsageRecord
+        fields = '__all__'
+
+
 class SubscriptionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     type = serializers.CharField(source='get_type')
