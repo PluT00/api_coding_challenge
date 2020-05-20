8eefa07cdebcb63fe1f76f585f5afcbbf1e13cd2
PluT00 <valeran2003@mail.ru>
2020-05-20 19:57:25 +0300

    Hotfix

--- wingtel/usage/tests/test_views.py
+++ wingtel/usage/tests/test_views.py
@@ -7,6 +7,6 @@ class ExceededSubsListAPIView(APITestCase):
 
 # Test response status code.
     def test_list_exceeded(self):
-        url = reverse('exceeded')
+        url = reverse('exceeded') + '?exceeded=10/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
